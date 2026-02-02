c967897e18e4638aba881fb101747352eb38db5c
aloyleow <aloyleow91@gmail.com>
2026-02-02 21:57:30 +0800

    feat: add map to escape terminal

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,11 +3,14 @@ vim.g.maplocalleader = "\\"
 
 local map = vim.keymap.set
 
+-- open terminal
 map("n", "<leader>t", function()
-	vim.cmd("10sp | term")
+  vim.cmd("8sp | term")
 end)
 
+map("t", "<leader><Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
+
+-- Formatting files
 map({ "n", "v" }, "<leader>f", function()
   require("conform").format()
 end, { desc = "Format file" })
-
